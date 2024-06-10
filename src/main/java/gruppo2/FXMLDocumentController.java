b5bae894a744543798346e5afee7e526442652a5
aaaaaaurora <auroracampione02@gmail.com>
2024-06-10 18:43:14 +0200

    alcuni commenti

--- src/main/java/gruppo2/FXMLDocumentController.java
+++ src/main/java/gruppo2/FXMLDocumentController.java
@@ -78,13 +78,22 @@ public class FXMLDocumentController implements Initializable {
         selezionaDocumento();
     }
 
+
+    // Gestisce la query inserita dall'utente
     @FXML
     private void handleQuery() {
         String queryText = queryTf.getText();
+
+        // Se la query è vuota, vengono mostrati tutti i documenti;
         if (queryText == null || queryText.trim().isEmpty()) {
             List<Document> allDocuments = new ArrayList<>(resultMapByDocument.keySet());
             tableView.setItems(observableArrayList(allDocuments));
         } else {
+            /* Altrimenti vengono mostrati solo i documenti filtrati in base alla similarità:
+            - pulisce la query dalle stopwords;
+            - crea un vettore dalla query e calcola la somiglianza tra la query e i documenti usando il coseno di similarità;
+            - ordina i documenti in base alla somiglianza;
+            - aggiorna infine la tabella per mostrare i risultati */
             String cleanedQuery = cleanAndRemoveStopwords(queryText);
             Map<String, Integer> queryVector = textToVector(cleanedQuery);
             corrispondenzaSimiliarita.clear();
@@ -101,12 +110,18 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
+
+    //  Converte il testo di un documento  in un vettore di frequenze delle parole
     private Map<String, Integer> textToVector(String text) {
         Map<String, Integer> vector = new TreeMap<>();
+
+        // ogni parola nel testo diventa una chiave nella mappa e il valore associato è il numero di occorrenze di quella parola
         Arrays.stream(text.split("\\s+")).forEach(word -> vector.merge(word, 1, Integer::sum));
         return vector;
     }
 
+
+    // Calcola il coseno di similarità tra due vettori: misura quanto sono simili due vettori
     private static double calculateCosineSimilarity(Map<String, Integer> vector1, Map<String, Integer> vector2) {
         double dotProduct = 0.0;
         double normA = 0.0;
@@ -128,22 +143,30 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
+
+    // Carica le stopwords da un file e le inserisce in una lista
     public void loadStopwords() throws IOException {
         File stopwordsFile = new File("stopwords-it.txt");
+
         if (stopwordsFile.exists()) {
             this.stopwords = Files.readAllLines(stopwordsFile.toPath());
         } else {
+            // Se il file non esiste, la lista di stopwords sarà vuota
             this.stopwords = new ArrayList<>();
             System.out.println("Stopwords file not found.");
         }
     }
 
+
     @FXML
     private void folderSelection(ActionEvent event) throws IOException {
+        // Viene creato un oggetto DirectoryChooser che permette all'utente di selezionare una cartella
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Seleziona una cartella");
         File selectedDirectory = directoryChooser.showDialog(null);
 
+
+        // Viene salvata la lista dei files presenti nella cartella selezionata
         if (selectedDirectory != null) {
             System.out.println("Cartella selezionata: " + selectedDirectory.getAbsolutePath());
             File[] files = selectedDirectory.listFiles();
@@ -152,14 +175,19 @@ public class FXMLDocumentController implements Initializable {
 
                 List<Future<Document>> futures = new ArrayList<>();
 
+                /* Per ogni file.txt presente nella cartella selezionata:
+                   - viene inviato un task all' executorService per leggere il testo del documento dal file;
+                   - tutti i risultati del task vengono aggiunti alla lista futures */
                 for (File file : files) {
                     if (file.isFile() && file.getName().endsWith(".txt")) {
                         futures.add(executorService.submit(() -> readDocumentFromFile(file)));
                     }
                 }
 
+                // Per ogni elemento della lista futures viene creato un oggetto Document, il quale se non è null, viene aggiunto alla lista documents
                 for (Future<Document> future : futures) {
                     try {
+                        // attende il completamento di tutte le operazioni asincrone inviate all'executorService
                         Document document = future.get();
                         if (document != null) {
                             documents.add(document);
@@ -169,6 +197,7 @@ public class FXMLDocumentController implements Initializable {
                     }
                 }
 
+                // Vengono creati il vocabolario e i vettori di frequenza delle parole per tutti i documenti presenti nella lista documents.
                 createVocabularyAndVectors(documents);
                 pane1.setVisible(false);
                 pane2.setVisible(true);
@@ -178,6 +207,8 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
+
+    // Legge un documento da un file
     private Document readDocumentFromFile(File file) {
         try (BufferedReader bfr = new BufferedReader(new FileReader(file))) {
             String title = bfr.readLine();
@@ -193,9 +224,15 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
+
+    // Crea il vocabolario e i vettori di documenti
     private void createVocabularyAndVectors(List<Document> documents) {
         List<Future<Void>> futures = new ArrayList<>();
 
+        /* Per ogni documento viene inviato un task all'executorService per:
+           - pulisce il testo rimuove le stopwords;
+           - aggiunge le parole del testo senza stopwords, al vocabolario;
+           - creare un vettore di frequenza delle parole */
         for (Document document : documents) {
             futures.add(executorService.submit(() -> {
                 String cleanedText = cleanAndRemoveStopwords(document.getTitle() + " " + document.getDocument_text());
@@ -215,19 +252,37 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
+
+    // Pulisce il testo del documento e rimuove le stopwrods
     private String cleanAndRemoveStopwords(String text) {
+        /* Pulizia:
+          - sostituisce tutti gli apostrofi con spazi;
+          - rimuove tutti i caratteri non alfabetici e non spazi (\p{L} è una proprietà Unicode che rappresenta tutte le lettere);
+          - converte tutto il testo in minuscolo per uniformità */
         String cleanedText = text.replaceAll("'", " ").replaceAll("[^\\p{L}\\s]", " ").toLowerCase();
+
+        /* Rimozione stopwrdos:
+           - divide il testo pulito in parole, usando gli spazi come delimitatori;
+           - filtra le parole, rimuovendo quelle presenti nella lista delle stopwords;
+           - colleziona le parole filtrate in una lista */
         List<String> words = Stream.of(cleanedText.split("\\s+")).filter(word -> !stopwords.contains(word)).collect(Collectors.toList());
+
+        // Unisce le parole rimanenti in una singola stringa separata da spazi.
         return String.join(" ", words);
     }
 
+
+    /* Divide il testo in parole usando gli spazi come delimitatori e aggiorna il vocabolario: per ogni parola aggiorna il relativo conteggio nel
+    vocabolario (aggiunge la parola al vocabolario con un conteggio di 1 se non è presente, altrimenti incrementa il conteggio di 1) */
     private void addWordsToVocabulary(String text) {
         Arrays.stream(text.split("\\s+")).forEach(parola -> vocabolario.merge(parola, 1, Integer::sum));
     }
 
+
+    // Quando viene selezionato un documento con un click, viene mostrato il suo contenuto
     private void selezionaDocumento() {
         tableView.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 1) { // Doppio click
+            if (event.getClickCount() == 1) { // singolo click
                 Document documentoSelezionato = tableView.getSelectionModel().getSelectedItem();
                 if (documentoSelezionato != null) {
                     mostraContenutoDocumento(documentoSelezionato);
@@ -236,6 +291,8 @@ public class FXMLDocumentController implements Initializable {
         });
     }
 
+
+    // Recupera il testo del documento selezionato e mostra anche le relative statistiche
     private void mostraContenutoDocumento(Document documentoSelezionato) {
         pane2.setVisible(false);
         paneDocumento.setVisible(true);
@@ -244,12 +301,16 @@ public class FXMLDocumentController implements Initializable {
         mostrastatisticheDocumento(documentoSelezionato);
     }
 
+
+    // Chiude il documento che era stato selezionato
     @FXML
     public void chiudiDocumento() {
         paneDocumento.setVisible(false);
         pane2.setVisible(true);
     }
 
+
+    // Calcola le statistiche sul documento selezionato
     @FXML
     public void mostrastatisticheDocumento(Document documentoSelezionato) {
         String testoDocumento = documentoSelezionato.getDocument_text();
